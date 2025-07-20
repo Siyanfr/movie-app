c75651c4bab35f0791abf490e66706b107aaf51f
Siyanfr <carosuscean@yahoo.com>
2025-07-20 05:10:54 +0000

    Jul 20, 2025, 1:10 PM

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 function Home (){
     const movies = [
+        {id: 1, title:"Joker", release_date: "2019"},
+        {id: 2, title:"Inception", release_date: "2010-07-16"},
+        {id: 3, title:"Interstellar", release_date: "2014-11-07"},
 
     ]
 
