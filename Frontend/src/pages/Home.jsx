ec492b082f8eb2cd4e1cae01b80d210979ef28ff
Siyanfr <carosuscean@yahoo.com>
2025-07-20 05:55:30 +0000

    Jul 20, 2025, 1:55 PM

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import MovieCard from "../components/MovieCard"
+import {useState} from 'react';
+
 function Home (){
+    const [searchquery, setSearchQuery] = useState("");
+
+
+
     const movies = [
         {id: 1, title:"Joker", release_date: "2019"},
         {id: 2, title:"Inception", release_date: "2010-07-16"},
         {id: 3, title:"Interstellar", release_date: "2014-11-07"},
     ]
 
-    const handleSearch = () => {
-
+    const handleSearch = () => 
+        e.preventDefault()
+        alert(`Searching for: ${searchquery}`);
     }
 
     return (
@@ -17,9 +24,12 @@ function Home (){
             type="text" 
             placeholder="Search for a movies..." 
             className="search-input" 
+            value = {searchquery}
+            onChange={(e) => setSearchQuery(e.target.value)}
             />
             <button type="submit" className="search-button">Search</button>
         </form>
+
         <div className="movies-grid">
             {movies.map((movie) => (
                 <MovieCard movie ={movie} key={movie.id}/>
