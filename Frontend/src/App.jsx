6bbd50aabc5d0d088f294400bf6e90953d778276
Siyanfr <you@example.com>
2025-07-20 04:49:23 +0000

    Jul 20, 2025, 12:49 PM

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,9 +3,15 @@ import MovieCard from './components/MovieCard';
 
 function App() {
   const movieNumber = 1;
+
+
   return (
   <>
-    <MovieCard movie={{title:"Inception", release_date: "2010-07-16"}} />
+    {movieNumber ===1 ?(
+      <MovieCard movie={{title:"Inception", release_date: "2010-07-16"}} />
+    ) : (
+      <MovieCard movie={{title:"Interstellar", release_date: "2014-11-07"}} />
+    )}
   </>
   );
 }
