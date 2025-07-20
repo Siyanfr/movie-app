bcaaffd8a115d2235f4575fbb79c928fa57b6d50
Siyanfr <carosuscean@yahoo.com>
2025-07-20 05:14:08 +0000

    Jul 20, 2025, 1:14 PM

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,19 @@
+import MovieCard from "../components/MovieCard"
 function Home (){
     const movies = [
         {id: 1, title:"Joker", release_date: "2019"},
         {id: 2, title:"Inception", release_date: "2010-07-16"},
         {id: 3, title:"Interstellar", release_date: "2014-11-07"},
-
     ]
 
-    return <div className="home"></div>
+
+    return <div className="home">
+        <div className="movies-grid">
+            {movies.map((movie) => (
+                <MovieCard movie ={movie} key={movie.id}/>
+            ))}
+        </div>
+    </div>
+
 }
 
