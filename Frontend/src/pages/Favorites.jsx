f798cd32cd1f72c4f98c85f481ff5ee3cd325ebd
Siyanfr <carosuscean@yahoo.com>
2025-07-21 13:40:42 +0000

    Jul 21, 2025, 9:40 PM

--- Frontend/src/pages/Favorites.jsx
+++ Frontend/src/pages/Favorites.jsx
@@ -1,8 +1,10 @@
-function Favorite(){
-    return <div className="favorite-empty">
+import "../css/Favorites.css";
+
+function Favorites(){
+    return <div className="favorites-empty">
         <h2> No Favorite Movies Yet</h2>
         <p>Start adding some movies to your favorites list!</p>
     </div>
 }
 
-export default Favorite;
+export default Favorites;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import MovieCard from "../components/MovieCard"
 import {useState} from 'react';
+import '../css/Home.css';
 
 function Home (){
     const [searchquery, setSearchQuery] = useState("");
-
-
-
+    
     const movies = [
         {id: 1, title:"Joker", release_date: "2019"},
         {id: 2, title:"Inception", release_date: "2010-07-16"},
