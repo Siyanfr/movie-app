54cb3c1cfe2dd35d8f8e579fbe5085ce9b85f3cf
Siyanfr <carosuscean@yahoo.com>
2025-07-21 14:25:49 +0000

    Jul 21, 2025, 10:25 PM

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -26,9 +26,27 @@ function Home (){
         loadPopularMovies()
     }, [])
        
-    const handleSearch = (e) => {
+    const handleSearch = async (e) => {
         e.preventDefault();
-        alert({searchquery});
+        if (!searchquery.trim()) return;
+        if (loading) return;
+        setLoading(true);
+        try{
+            const searchResults = await searchMovies(searchQuery);
+            setMovies(searchResults);
+            setError(null);
+        } catch (err) {
+            console.log(err);
+            setError("Failed to search movies");
+        } finally{
+            setLoading(false);
+        }
+
+        
+
+
+
+
         setSearchQuery("");
     };
 
@@ -45,14 +63,18 @@ function Home (){
             <button type="submit" className="search-button">Search</button>
         </form>
 
+        {error && <div className="error-message">{error}</div>}
+
+        {loading ? (
+            <div className="loading">Loading...</div> 
+        ):(
         <div className="movies-grid">
             {movies.map((movie) => (
-                (
                 <MovieCard movie ={movie} key={movie.id}/>
-                )
             ))}
         </div>
-    </div>
+        )}
+        </div>
     );
 }
 
