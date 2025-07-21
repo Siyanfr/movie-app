0c97d6379fd48ed85bbc6c223ee219cb42631841
Siyanfr <carosuscean@yahoo.com>
2025-07-21 13:50:19 +0000

    Jul 21, 2025, 9:50 PM

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,2 +1,8 @@
 const API_KEY = "055acd22c184b29e882307e1c5de0f47";
 const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
